19dbe9d977f3f3f0a2d6e6598a8fa6e600351134
Linh Nguyen <ptl.nguyen.03@gmail.com>
2026-08-20 10:46:52 +0200

    batman

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "8086 disassembler";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      strictDeps = true;
+
+      packages = with pkgs; [
+        rustc
+        cargo
+        rustfmt
+        clippy
+        rust-analyzer
+
+        gcc
+        pkg-config
+        llvmPackages.bintools
+
+        lldb
+
+        cargo-watch
+        cargo-nextest
+      ];
+
+      env = {
+        RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+        RUST_BACKTRACE = "1";
+      };
+    };
+  };
+}
